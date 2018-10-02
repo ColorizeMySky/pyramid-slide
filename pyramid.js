0f2004d02a78bfb649ecf50d502062ef7e63b997
ColorizeMySky <firiziant@gmail.com>
2018-10-02 04:20:03 +0300

    Implement with jQuery

--- pyramid.js
+++ pyramid.js
@@ -1,31 +1,33 @@
-let symbolHolder = document.querySelector('#symbol');
-let heigthHolder = document.querySelector('#heigth');
+let symbolHolder = $('#symbol');
+let heigthHolder = $('#heigth');
 
 drawPyramid();
 
-function drawPyramid() {
+symbolHolder.change(drawPyramid);
+heigthHolder.change(drawPyramid);
+
+
+function drawPyramid() {    
 	// remove all children elements
-	let placeForDraw = document.querySelector('#construction');	
-	while (placeForDraw.firstChild) {
-		placeForDraw.removeChild(placeForDraw.firstChild);
-	}
-	
-	let heigth = heigthHolder.value;	
-	let symbol = symbolHolder[symbolHolder.selectedIndex].value;
-	let strDraw = symbol;
+    let workingBlock = $("#construction");
+    workingBlock.find('*').remove();
 
-	for (var i = 1; i <= heigth; i++) {
-    	var space = '';
-    	for (var j = heigth - i - 1; j >= 0; j--) {
-    		space += ' ';
-    	}
-    	strDraw += symbol;
 
-    	let paragraf = document.createElement('p');
-    	placeForDraw.appendChild(paragraf);
-    	paragraf.innerHTML = space + strDraw;
+    let height = $( 'input[type="range"]' ).val();
+    
+
+    for (var i = 1; i <= height; i++) {
+        let paragraf = $('<p></p>');
+        paragraf.appendTo($('#construction'));
+
+        for (var k = height - i; k > 0; k--) {
+            let space = $('<span class="transparent"></span>');
+            space.appendTo(paragraf);
+        }
+
+        for (var j = 0; j < i + 1; j++) {
+            let symbol = $('<span>' +  $('select').val() + '</span>');
+            symbol.appendTo(paragraf);
+        }
     }
-
-    symbolHolder.addEventListener("change", drawPyramid);
-    heigthHolder.addEventListener("change", drawPyramid);
 }
